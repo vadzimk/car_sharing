2a9ad2769bfe4dbf33c25187defc6febd3ece329
Vadzim <vadzimk@hotmail.com>
2021-07-29 21:58:04 -0700

    complete header component

--- client/src/components/ui/Header.js
+++ client/src/components/ui/Header.js
@@ -12,7 +12,7 @@ import {
   SwipeableDrawer,
   List,
   ListItem,
-  ListItemText
+  ListItemText,
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -25,27 +25,27 @@ const useStyles = makeStyles(theme => ({ // get access to the theme properties
   toolbarMargin: {
     ...theme.mixins.toolbar,
     marginBottom: '1em',
-    [theme.breakpoints.down('md')]:{
-      marginBottom: 0
+    [theme.breakpoints.down('md')]: {
+      marginBottom: 0,
     },
-    [theme.breakpoints.down('xs')]:{
-      marginBottom: '-0.25em'
-    }
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: '-0.25em',
+    },
   },
   logo: {
     height: '5em',
-    [theme.breakpoints.down('md')]:{
-      height: '4em'
+    [theme.breakpoints.down('md')]: {
+      height: '4em',
     },
-    [theme.breakpoints.down('xs')]:{
-      height: '3.5em'
-    }
+    [theme.breakpoints.down('xs')]: {
+      height: '3.5em',
+    },
   },
   logoContainer: {
     padding: '0',
     '&:hover': {
-      backgroundColor: 'transparent'
-    }
+      backgroundColor: 'transparent',
+    },
   },
   tabContainer: {
     marginLeft: 'auto',
@@ -61,12 +61,24 @@ const useStyles = makeStyles(theme => ({ // get access to the theme properties
   drawerIconContainer: {
     marginLeft: 'auto',
     '&:hover': {
-      backgroundColor: 'transparent'
+      backgroundColor: 'transparent',
     },
   },
-  drawer:{
-  backgroundColor: theme.palette.primary.main
-  }
+  drawer: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  drawerItem: {
+    opacity: '0.7',
+  },
+  drawerItemSelected: {
+    opacity: 1,
+  },
+  drawerItemTextSelected: {
+    paddingBottom: '2px',
+    borderBottomStyle: 'solid',
+    borderBottomColor: theme.palette.secondary.main,
+  },
+
   
 }));
 
@@ -82,9 +94,8 @@ export default function Header () {
   const [location, setLocation] = useState('/');
   
   const {pathname} = useLocation();
-  
+  const newLocation = '/'.concat(pathname.split('/')[1]);
   useEffect(() => {
-    const newLocation = '/'.concat(pathname.split('/')[1]);
     setLocation(newLocation);
   }, [pathname]);
   
@@ -98,8 +109,9 @@ export default function Header () {
       <Tabs value={location} onChange={handleChange}
             className={classes.tabContainer}>
         {
-          routes.map((item)=>(
-            <Tab label={item.label} component={Link} to={item.path} value={item.path}
+          routes.map((item) => (
+            <Tab label={item.label} component={Link} to={item.path}
+                 value={item.path}
                  className={classes.tab} key={item.label}/>
           ))
         }
@@ -113,37 +125,49 @@ export default function Header () {
   const drawer = (
     <>
       <SwipeableDrawer disableBackdropTransition={!iOS} disableDiscovery={iOS}
-                       open={openDrawer} onClose={() => setOpenDrawer(false)} onOpen={() => setOpenDrawer(true)}
+                       open={openDrawer} onClose={() => setOpenDrawer(false)}
+                       onOpen={() => setOpenDrawer(true)}
                        classes={{paper: classes.drawer}}
+                       anchor="right"
       >
         <List disablePadding>
-        {
-          routes.map((item)=>(
-          <ListItem component={Link} to={item.path} divider button onClick={() => setOpenDrawer(false)} key={item.label}>
-            <ListItemText disableTypography>
-              {item.label}
-            </ListItemText>
-          </ListItem>
-          ))
-        }
+          {
+            routes.map((item) => (
+              <ListItem component={Link} to={item.path} divider button
+                        onClick={() => setOpenDrawer(false)}
+                        key={item.label}
+                        selected={location === item.path}
+                        className={location === item.path ?
+                          classes.drawerItemSelected:
+                          classes.drawerItem}
+              >
+                <ListItemText disableTypography
+                              className={location === item.path &&
+                              classes.drawerItemTextSelected}>
+                  {item.label}
+                </ListItemText>
+              </ListItem>
+            ))
+          }
         </List>
       </SwipeableDrawer>
-      <IconButton onClick={() => setOpenDrawer(!openDrawer)} disableRipple className={classes.drawerIconContainer}>
+      <IconButton onClick={() => setOpenDrawer(!openDrawer)} disableRipple
+                  className={classes.drawerIconContainer}>
         <MenuIcon/>
       </IconButton>
     </>
   );
   
-  
   return (
     <>
-      <AppBar position="absolute">
+      <AppBar position="absolute" >
         <Toolbar>
-          <Button component={Link} to="/home" className={classes.logoContainer} disableRipple>
+          <Button component={Link} to="/home" className={classes.logoContainer}
+                  disableRipple>
             <img src={logo} alt="company logo" className={classes.logo}/>
           </Button>
           {
-            matches ? drawer : menu
+            matches ? drawer:menu
           }
         </Toolbar>
       </AppBar>
