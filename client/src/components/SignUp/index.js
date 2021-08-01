eab73507c826fab542dc90a78de986b010bf5a8d
Vadzim <vadzimk@hotmail.com>
2021-07-31 21:39:49 -0700

    centered form

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -38,73 +38,95 @@ const SignUp = () => {
     };
     
     return (
-      <div>
-        <Typography variant="h4">Sign up</Typography>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={onSubmit}
-        >
-          {
-            ({isValid, dirty, errors, handleChange}) => {
-              console.dir('errors', errors);
-              
-              return (
-                
-                <GridContainer className={classes.mainContainer}
-                               direction="column"
-                               justifyContent="flex-start"
-                               alignItems="center"
-                >
-                  
-                  
-                  <GridItem>
-                    <TextField
-                      label="First Name"
-                      name="first_name"
-                      required
-                      error
-                      helperText={errors.first_name}
-                      onChange={handleChange}
-                    />
-                  </GridItem>
-                  <GridItem>
-                    <TextField
-                      label="* Last Name"
-                      name="last_name"
-                    />
-                  </GridItem>
-                  <GridItem>
-                    <TextField
-                      label="* Drivers license"
-                      name="dl_number"
-                    />
-                  </GridItem>
-                  <GridItem>
-                    <KeyboardDatePicker
-                      label="* Date of issue"
-                      name="dl_date"
-                      disableFuture
-                      openTo="year"
-                      format="dd/MM/yyyy"
-                    />
-                  </GridItem>
-                  <GridItem>
-                    <button
-                      type="submit"
-                      color="primary"
-                      variant="contained"
-                      disabled={!dirty || !isValid}
-                    >
-                      Sign up
-                    </button>
-                  </GridItem>
-                </GridContainer>
-              );
-            }
-          }
-        </Formik>
-      </div>
+      <GridContainer direction="row" justifyContent='center'>
+        <GridItem> {/*single column*/}
+          <GridContainer direction="column" justifyContent='flex-start'>
+            <GridItem>
+              <Typography variant="h4">Sign up</Typography>
+            </GridItem>
+            <GridItem>
+              <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
+              >
+                {
+                  ({isValid, dirty, errors, handleChange}) => {
+                    console.dir('errors', errors);
+                    
+                    return (
+                      
+                      <GridContainer className={classes.mainContainer}
+                                     direction="column"
+                                     justifyContent="flex-start"
+                                     alignItems="stretch"
+                                     spacing={1}
+                      >
+                        
+                        
+                        <GridItem>
+                          <TextField
+                            // label="First Name"
+                            name="first_name"
+                            fullWidth
+                            // InputLabelProps={{disabled:true}}
+                            required
+                            error={errors.first_name}
+                            helperText={errors.first_name}
+                            onChange={handleChange}
+                            
+                          />
+                        </GridItem>
+                        <GridItem>
+                          <TextField
+                            label="Last Name"
+                            name="last_name"
+                            fullWidth
+                            required
+                            error={errors.last_name}
+                            helperText={errors.last_name}
+                            onChange={handleChange}
+                          />
+                        </GridItem>
+                        <GridItem>
+                          <TextField
+                            label="Drivers license"
+                            name="dl_number"
+                            fullWidth
+                            required
+                            error={errors.dl_number}
+                            helperText={errors.dl_number}
+                            onChange={handleChange}
+                          />
+                        </GridItem>
+                        <GridItem>
+                          <KeyboardDatePicker
+                            label="* Date of issue"
+                            name="dl_date"
+                            disableFuture
+                            openTo="year"
+                            format="dd/MM/yyyy"
+                          />
+                        </GridItem>
+                        <GridItem>
+                          <button
+                            type="submit"
+                            color="primary"
+                            variant="contained"
+                            disabled={!dirty || !isValid}
+                          >
+                            Sign up
+                          </button>
+                        </GridItem>
+                      </GridContainer>
+                    );
+                  }
+                }
+              </Formik>
+            </GridItem>
+          </GridContainer>
+        </GridItem>
+      </GridContainer>
     
     );
   }
