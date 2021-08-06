1c6111d24b0babe2d3193a7028409cb2dfde68c6
Vadzim <vadzimk@hotmail.com>
2021-08-06 11:23:25 -0700

    configure remote db connection

--- server/config.js
+++ server/config.js
@@ -4,7 +4,12 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT || 3001,
-  db: {},
+  db: {
+    connectionString: process.env.DATABASE_URL,
+    // ssl: {
+    //   rejectUnauthorized: false
+    // }
+  },
   aws: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -12,18 +17,20 @@ const config = {
   }
 };
 
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-  config.db.user = process.env.DEV_DB_USER;
-  config.db.host = process.env.DEV_DB_HOST;
-  config.db.database = process.env.DEV_DB_DB;
-  config.db.password = process.env.DEV_DB_PASSWORD;
-  config.db.port = process.env.DEV_DB_PORT;
-} else {
-  config.db.connectionString = process.env.DATABASE_URL;
-  config.db.ssl =  {
-    rejectUnauthorized: false
-  };
-}
+
+
+// if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+//   config.db.user = process.env.DEV_DB_USER;
+//   config.db.host = process.env.DEV_DB_HOST;
+//   config.db.database = process.env.DEV_DB_DB;
+//   config.db.password = process.env.DEV_DB_PASSWORD;
+//   config.db.port = process.env.DEV_DB_PORT;
+// } else {
+//   config.db.connectionString = process.env.DATABASE_URL;
+//   config.db.ssl =  {
+//     rejectUnauthorized: false
+//   };
+// }
 
 
 export default config;
