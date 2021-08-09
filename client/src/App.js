4064631f29ee31d2c83ddae5db55ac9903584445
Vadzim <vadzimk@hotmail.com>
2021-08-08 17:08:06 -0700

    make footer stick to the bottom maintaining dynamic height

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,45 @@
 import React, {useEffect} from 'react';
-
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {Container, ThemeProvider} from '@material-ui/core';
-import Header from './components/ui/Header.js';
 import theme from './Theme.js';
 import routes from './routes.js';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {ThemeProvider, Container} from '@material-ui/core';
+
+import Header from './components/ui/Header.js';
 import Footer from './components/ui/Footer.js';
 import Notification from './components/ui/Notification.js';
 import {useDispatch} from 'react-redux';
 import {getUserFromStorage} from './reducers/userReducer.js';
 
+
 const App = () => {
-  
+const style = {
+  mainContainer:{
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  mainContent:{
+    flex: '1 0 auto'
+  },
+  footer:{
+    flexShrink: 0,
+  },
+};
+
   const dispatch = useDispatch();
   
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(getUserFromStorage());
   });
   
   return (
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Container>
-            <Header/>
-            <Notification/>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div style={style.mainContainer}>
+          
+          <Header/>
+          <Notification/>
+          <Container style={style.mainContent}>
             <Switch>
               {
                 routes.map((item) => (
@@ -33,9 +49,10 @@ const App = () => {
               }
             </Switch>
           </Container>
-          <Footer/>
-        </Router>
-      </ThemeProvider>
+          <Footer style={style.footer}/>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 };
 
