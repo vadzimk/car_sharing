53e2ad228870c750196ecefc11dbb027a901697a
Vadzim <vadzimk@hotmail.com>
2021-09-07 21:17:31 -0700

    fix listings/create

--- client/src/components/HostRoute.js
+++ client/src/components/HostRoute.js
@@ -7,12 +7,33 @@ const HostRoute = ({children, ...rest}) => {
   
   const user = useSelector(state => state.user);
   
+  // return (
+  //   <Route {...rest} >
+  //     {user?.ishost ? children:
+  //       (user ? <Redirect to="/"/>:
+  //         <Redirect to="/login"/>)}
+  //   </Route>
+  // );
   return (
-    <Route {...rest} >
-      {user?.ishost ? children:
-        (user ? <Redirect to="/"/>:
-          <Redirect to="/login"/>)}
-    </Route>
+    <Route
+      {...rest}
+      render={({location}) =>
+        user?.ishost ? (
+            children
+          ):
+          user ? (<Redirect to={{
+            pathname: '/',
+            state: {from: location},
+          }}/>):(
+            <Redirect
+              to={{
+                pathname: '/login',
+                state: {from: location},
+              }}
+            />
+          )
+      }
+    />
   );
 };
 
