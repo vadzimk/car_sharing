e96e9c005547f7a5d299f0c18d539a59be456b86
Vadzim <vadzimk@hotmail.com>
2021-10-16 13:38:03 -0700

    leaflet map, positioning issue

--- client/src/components/HomeMap/Map.js
+++ client/src/components/HomeMap/Map.js
@@ -1,40 +1,56 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
-import {makeStyles} from '@mui/styles';
 import 'leaflet/dist/leaflet.css';
 import provider from '../../leaflet/provider.js';
 
-const useStyles = makeStyles(() => ({
-  mapContainer: {
-    width: '100%',
-    height: '400px',
-  },
-}));
+// const useStyles = makeStyles(() => ({
+//   mapContainer: {
+//     width: '100%',
+//     minHeight: '100%',
+//   },
+// }));
 
 const Map = () => {
-  const classes = useStyles();
+  const [height, setHeight] = useState(0);
+  
+  const ref = useRef(null);
+  
+  useEffect(() => {
+    setHeight(ref.current.clientHeight);
+  });
+  
+  console.log('height', height);
   
   // eslint-disable-next-line no-unused-vars
-  const [center, setCenter] = useState({lat:51.505, lng: -0.09});
+  const [center, setCenter] = useState({lat: 51.505, lng: -0.09});
+  
+  
+  // overflow hidden with 100vh hides the attribution line and the bottom of the map
+  // but solves the question of resizing the window
+  // https://www.pluralsight.com/guides/re-render-react-component-on-window-resize
+  // maybe anchor it at the bottom right corner
   return (
-    <div>
-      <MapContainer
-        center={center}
-        zoom={13}
-        scrollWheelZoom={false}
-        className={classes.mapContainer}
-      >
-        <TileLayer
-          attribution={provider.attribution}
-          url={provider.url}
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br/> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+    <div ref={ref} style={{height: '100%', width: '100%'}}>
+      <div style={{height: `${height}px`, width: '100%', overflow: 'hidden'}}>
+        <MapContainer
+          center={center}
+          zoom={13}
+          scrollWheelZoom={false}
+          style={{width: '100%', height: '100vh'}}
+        >
+          <TileLayer
+            attribution={provider.attribution}
+            url={provider.url}
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br/> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </div>
+  
   );
 };
 
--- client/src/components/HomeMap/index.js
+++ client/src/components/HomeMap/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import Map from './Map.js';
+import {makeStyles} from '@mui/styles';
+import {GridContainer, GridItem} from '../ui/GridRenamed.js';
 
-const HomeMap = ()=>{
-  return(
-    <div>
-      Map
-      <Map/>
-    </div>
+const useStyles = makeStyles(() => ({
+  container: {
+    width: '100%',
+    height: '100%',
+
+  },
+}));
+
+const HomeMap = () => {
+  const classes = useStyles();
+
+  return (
+
+      <GridContainer
+        justifyContent="space-between"
+        alignItems="stretch"
+        className={classes.container}
+      >
+        <GridItem >
+          images
+        </GridItem>
+        <GridItem style={{flexGrow: 1}}>
+          <Map/>
+        </GridItem>
+      </GridContainer>
+
   );
 };
 
