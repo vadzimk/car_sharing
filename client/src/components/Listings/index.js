771bda5c5583ea0235e7b6f263578045f364a0b6
Vadzim <vadzimk@hotmail.com>
2021-10-18 01:12:41 -0700

    style responsive listings component

--- client/src/components/Listings/index.js
+++ client/src/components/Listings/index.js
@@ -5,12 +5,27 @@ import {useDispatch, useSelector} from 'react-redux';
 import {getHostListings} from '../../reducers/listingsReducer.js';
 import {Button, TextField} from '@mui/material';
 import ListingTable from './ListingTable.js';
-import {LocalizationProvider, DatePicker } from '@mui/lab';
+import {LocalizationProvider, DatePicker} from '@mui/lab';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import {makeStyles} from '@mui/styles';
+
+const useStyles = makeStyles((theme)=>({
+  newListingButton:{
+    margin: '8px',
+    [theme.breakpoints.down('sm')]:{
+      width: '100%',
+    },
+    [theme.breakpoints.up('sm')]:{
+      width: '180px'
+    }
+  }
+}));
 
 const Listings = () => {
+  const classes = useStyles();
   const date = new Date();
-  const [dateFrom, setDateFrom] = useState(new Date(date.getFullYear(), date.getMonth(), 1));
+  const [dateFrom, setDateFrom] = useState(
+    new Date(date.getFullYear(), date.getMonth(), 1));
   const [dateTo, setDateTo] = useState(new Date());
   
   const history = useHistory();
@@ -18,7 +33,7 @@ const Listings = () => {
   const handleNewListing = () => {
     history.push('/listings/new');
   };
-
+  
   const dispatch = useDispatch();
   
   const hostListings = useSelector(state => state.listings);
@@ -27,57 +42,67 @@ const Listings = () => {
     dispatch(getHostListings(dateFrom, dateTo));
   }, [dateFrom, dateTo]);
   
-  
   // https://material-ui.com/components/data-grid/editing
   return (
     <GridContainer direction="column">
       <GridItem>
-        <GridContainer direction="row" justifyContent="space-between" alignItems="center" spacing={1}>
-          <GridItem>
-            <Button onClick={handleNewListing} variant="contained"
-                    color="secondary">NEW LISTING</Button>
-          </GridItem>
-          <GridItem>
+        <GridContainer direction="row" justifyContent="space-between"
+                       alignItems="center" spacing={1}>
+          <GridContainer style={{flexGrow: 3, width: '180px'}} justifyContent="space-between">
+            <GridItem style={{width: '180px', flexGrow: 1}}>
+              <Button onClick={handleNewListing} variant="contained"
+                      color="secondary"
+                      className={classes.newListingButton}
+              >
+                NEW LISTING
+              </Button>
+            </GridItem>
+            <GridItem style={{width: '180px', flexGrow:1}}></GridItem>
+          </GridContainer>
+          <GridItem style={{flexGrow: 1}}>
             <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <GridContainer direction="row" spacing={1} justifyContent="flex-end">
-              <GridItem
-                style={{maxWidth: '180px'}}
-              >
-                <DatePicker
-                  autoOk={true}
-                  disableToolbar
-                  variant="inline"
-                  format="MM/dd/yyyy"
-                  margin="normal"
-                  label="From"
-                  value={dateFrom}
-                  onChange={(date)=>setDateFrom(date)}
-                  KeyboardButtonProps={{
-                    'aria-label': 'change date',
-                  }}
-                  renderInput={(params) => <TextField {...params} />}
-                />
-              </GridItem>
-              <GridItem
-                style={{maxWidth: '180px'}}
-              >
-                <DatePicker
-                  autoOk={true}
-                  disableToolbar
-                  variant="inline"
-                  format="MM/dd/yyyy"
-                  margin="normal"
-                  label="To"
-                  value={dateTo}
-                  onChange={(date)=>setDateTo(date)}
-                  KeyboardButtonProps={{
-                    'aria-label': 'change date',
-                  }}
-                  renderInput={(params) => <TextField {...params} />}
-                />
-              </GridItem>
+              <GridContainer direction="row" spacing={1}
+                             justifyContent="flex-end">
+                <GridItem
+                  style={{flexGrow: 1, width: '180px'}}
+                >
+                  <DatePicker
+                    autoOk={true}
+                    disableToolbar
+                    variant="inline"
+                    format="MM/dd/yyyy"
+                    margin="normal"
+                    label="From"
+                    value={dateFrom}
+                    onChange={(date) => setDateFrom(date)}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date',
+                    }}
+                    renderInput={(params) => <TextField
+                      fullWidth {...params} />}
+                  />
+                </GridItem>
+                <GridItem
+                  style={{flexGrow: 1, width: '180px'}}
+                >
+                  <DatePicker
+                    autoOk={true}
+                    disableToolbar
+                    variant="inline"
+                    format="MM/dd/yyyy"
+                    margin="normal"
+                    label="To"
+                    value={dateTo}
+                    onChange={(date) => setDateTo(date)}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date',
+                    }}
+                    renderInput={(params) => <TextField
+                      fullWidth {...params} />}
+                  />
+                </GridItem>
               
-            </GridContainer>
+              </GridContainer>
             </LocalizationProvider>
           </GridItem>
         </GridContainer>
