5f14348fcc40aef950ab98c480ea11109b4b0131
Vadzim <vadzimk@hotmail.com>
2022-07-02 19:49:22 -0700

    replace s3 storage with oci storage

--- server/config.js
+++ server/config.js
@@ -16,6 +16,12 @@ const config = {
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION,
   },
+  oci: {
+    namespaceName: process.env.OCI_NAMESPACE,
+    bucketName: process.env.OCI_BUCKET,
+    tenancy: process.env.OCI_CLI_TENANCY,
+    compartment: process.env.OCI_COMPARTMENT,
+  }
 };
 
 if ((process.env.NODE_ENV === 'development' || process.env.NODE_ENV ===
