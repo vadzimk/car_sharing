8f63b2761b8cc34a47606a8f2f4e7a64780aec73
Vadzim <vadzimk@hotmail.com>
2022-07-03 21:35:59 -0700

    update ci

--- server/config.js
+++ server/config.js
@@ -24,8 +24,8 @@ const config = {
   }
 };
 
-if ((process.env.NODE_ENV === 'development' || process.env.NODE_ENV ===
-  'test') && !process.env.GITHUB_ACTIONS) {
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV ===
+  'test') {
   config.db = {connectionString: process.env.DATABASE_URL_TEST};
 } 
 if(process.env.GITHUB_ACTIONS){
